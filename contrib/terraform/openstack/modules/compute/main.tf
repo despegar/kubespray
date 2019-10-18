d1a370410500e752f5b13dff24508b5b259c0319
Gustavo Randich <grandich@team-controller-101.team.despexds.net>
2019-10-18 16:56:15 -0400

    rr

--- contrib/terraform/openstack/modules/compute/main.tf
+++ contrib/terraform/openstack/modules/compute/main.tf
@@ -334,6 +334,19 @@ resource "openstack_compute_instance_v2" "etcd_custom_volume_size" {
 }
 
 # calico-rr
+variable "subnet_ids" {
+  type        = list(string)
+  default     = ["d85e2b9e-f9e2-45e5-82d4-8990830cac49","0ba57227-231e-486a-a196-a2ae5c0521c6"]
+}
+variable "reflectors" {
+  type        = list(string)
+  default     = ["10.184.42.241", "10.184.46.241"]
+}
+variable "subnets" {
+  type        = list(string)
+  default     = ["10.184.42.0", "10.184.46.0"]
+}
+
 resource "openstack_networking_secgroup_v2" "rr" {
   name                 = "${var.cluster_name}-rr"
   description          = "${var.cluster_name} - Calico Route Reflector"
@@ -352,14 +365,14 @@ resource "openstack_networking_secgroup_rule_v2" "rr" {
 
 resource "openstack_networking_port_v2" "k8s_calico_rr_no_floating_ip" {
   name           = "${var.cluster_name}-k8s-calico-rr-nf-${count.index+1}"
-  count          = "${var.number_of_calico_rr_no_floating_ip}"
+  count          = length(var.subnet_ids)
   admin_state_up = "true"
 
   network_id     = "${var.provider_network_id}"
   # neutron/policy.json get_port, create_port:fixed_ips:subnet_id y create_port:fixed_ips:ip_address a ""
   fixed_ip {
-     subnet_id  = "d85e2b9e-f9e2-45e5-82d4-8990830cac49"
-     ip_address = "10.184.42.241" # 10.184.42.240/29 
+     subnet_id  = var.subnet_ids[count.index]
+     ip_address =  var.reflectors[count.index]
   }
 
   security_group_ids = ["${openstack_networking_secgroup_v2.k8s.id}",
@@ -368,7 +381,7 @@ resource "openstack_networking_port_v2" "k8s_calico_rr_no_floating_ip" {
 
 resource "openstack_compute_instance_v2" "k8s_calico_rr_no_floating_ip" {
   name              = "${var.cluster_name}-k8s-calico-rr-nf-${count.index+1}"
-  count             = "${var.number_of_calico_rr_no_floating_ip}"
+  count             = length(var.subnet_ids)
   availability_zone = "${element(var.az_list, count.index)}"
   image_name        = "${var.image}"
   flavor_id         = "${var.flavor_calico_rr}"
@@ -382,7 +395,7 @@ resource "openstack_compute_instance_v2" "k8s_calico_rr_no_floating_ip" {
     ssh_user         = "${var.ssh_user}"
     kubespray_groups = "calico-rr,k8s-cluster,no-floating,${var.supplementary_node_groups}"
     depends_on       = "${var.network_id}"
-    cluster_id       = "10.184.42.0" # usamos la subnet para que todos los RR de un mismo rack formen un cluster
+    cluster_id       = var.subnets[count.index]
   }
 }
 
