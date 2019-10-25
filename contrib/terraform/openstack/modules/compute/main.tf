2d735efe91588c684587add934090fe0168d74f1
Gustavo Randich <grandich@despegar.com>
2019-10-25 11:49:58 -0300

    icmp

--- contrib/terraform/openstack/modules/compute/main.tf
+++ contrib/terraform/openstack/modules/compute/main.tf
@@ -78,6 +78,24 @@ resource "openstack_networking_secgroup_rule_v2" "egress" {
   security_group_id = "${openstack_networking_secgroup_v2.k8s.id}"
 }
 
+resource "openstack_networking_secgroup_rule_v2" "k8s_icmp_i" {
+  count             = "${length(var.k8s_allowed_remote_ips)}"
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "icmp"
+  remote_ip_prefix  = "${var.k8s_allowed_remote_ips[count.index]}"
+  security_group_id = "${openstack_networking_secgroup_v2.k8s.id}"
+}
+
+resource "openstack_networking_secgroup_rule_v2" "k8s_icmp_e" {
+  count             = "${length(var.k8s_allowed_remote_ips)}"
+  direction         = "eggress"
+  ethertype         = "IPv4"
+  protocol          = "icmp"
+  remote_ip_prefix  = "${var.k8s_allowed_remote_ips[count.index]}"
+  security_group_id = "${openstack_networking_secgroup_v2.k8s.id}"
+}
+
 resource "openstack_networking_secgroup_v2" "worker" {
   name                 = "${var.cluster_name}-k8s-worker"
   description          = "${var.cluster_name} - Kubernetes worker nodes"
