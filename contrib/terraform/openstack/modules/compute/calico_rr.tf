66b1ddb0c6d7ffcb979a74345194d4b8fece67b1
Gustavo Randich <gustavo.randich@gmail.com>
2019-12-02 12:29:25 -0300

    rr

--- contrib/terraform/openstack/modules/compute/calico_rr.tf
+++ contrib/terraform/openstack/modules/compute/calico_rr.tf
@@ -49,23 +49,23 @@ resource "openstack_networking_port_v2" "k8s_calico_rr_no_floating_ip" {
    ]
 }
 
-resource "openstack_compute_instance_v2" "k8s_calico_rr_no_floating_ip" {
-  name              = "${var.cluster_name}-k8s-calico-rr-nf-${count.index+1}"
-  count             = length(var.subnet_ids)
-  availability_zone = "nova:${var.reflector_hosts[count.index]}"
-  image_name        = "${var.image}"
-  flavor_id         = "${var.flavor_calico_rr}"
-  key_pair          = "${openstack_compute_keypair_v2.k8s.name}"
+# resource "openstack_compute_instance_v2" "k8s_calico_rr_no_floating_ip" {
+#   name              = "${var.cluster_name}-k8s-calico-rr-nf-${count.index+1}"
+#   count             = length(var.subnet_ids)
+#   availability_zone = "nova:${var.reflector_hosts[count.index]}"
+#   image_name        = "${var.image}"
+#   flavor_id         = "${var.flavor_calico_rr}"
+#   key_pair          = "${openstack_compute_keypair_v2.k8s.name}"
 
-  network {
-    port = "${element(openstack_networking_port_v2.k8s_calico_rr_no_floating_ip.*.id, count.index)}"
-  }
+#   network {
+#     port = "${element(openstack_networking_port_v2.k8s_calico_rr_no_floating_ip.*.id, count.index)}"
+#   }
 
-  metadata = {
-    ssh_user         = "${var.ssh_user}"
-    kubespray_groups = "calico-rr,k8s-cluster,no-floating,${var.supplementary_node_groups}"
-    depends_on       = "${var.network_id}"
-    cluster_id       = var.subnets[count.index]
-    node_taints      = "calico-rr=true:NoSchedule"
-  }
-}
+#   metadata = {
+#     ssh_user         = "${var.ssh_user}"
+#     kubespray_groups = "calico-rr,k8s-cluster,no-floating,${var.supplementary_node_groups}"
+#     depends_on       = "${var.network_id}"
+#     cluster_id       = var.subnets[count.index]
+#     node_taints      = "calico-rr=true:NoSchedule"
+#   }
+# }
