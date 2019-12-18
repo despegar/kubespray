4f6d9bfc0732959ce567d40b6727b5f4465dbd6c
Gustavo Randich <grandich@despegar.com>
2019-12-18 12:45:24 -0300

    test rr

--- contrib/terraform/openstack/modules/compute/calico_rr.tf
+++ contrib/terraform/openstack/modules/compute/calico_rr.tf
@@ -31,3 +31,27 @@ resource "openstack_networking_port_v2" "k8s_calico_rr_no_floating_ip" {
      "${openstack_networking_secgroup_v2.rr.id}"
    ]
 }
+
+resource "openstack_compute_instance_v2" "k8s_calico_rr_no_floating_ip" {
+  name              = "${var.cluster_name}-k8s-calico-rr-nf-${var.calico_rr_ass[count.index]}"
+  count             = length(var.calico_rr_azs)
+  availability_zone = "${var.calico_rr_azs[count.index]}"
+  image_name        = "${var.image}"
+  flavor_id         = "${var.flavor_calico_rr}"
+  key_pair          = "${openstack_compute_keypair_v2.k8s.name}"
+
+  network {
+    port = "${element(openstack_networking_port_v2.k8s_calico_rr_no_floating_ip.*.id, count.index)}"
+  }
+
+  metadata = {
+    ssh_user         = "${var.ssh_user}"
+    kubespray_groups = "calico-rr,k8s-cluster,no-floating,${var.supplementary_node_groups}"
+    depends_on       = "${var.network_id}"
+    cluster_id       = var.calico_rr_cluster_ids[count.index]
+    node_taints      = "calico-rr=true:NoSchedule"
+    AS               = var.calico_rr_ass[count.index]
+    RR               = var.calico_rr_ips[count.index]
+    TOR              = var.calico_rr_tors[count.index]
+  }
+}
