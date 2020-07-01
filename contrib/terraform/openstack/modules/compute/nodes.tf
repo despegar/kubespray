22756479e9873f7dcd55949882b5676164bc8124
Gustavo Randich <gustavo.randich@gmail.com>
2020-07-01 19:07:58 -0300

    refactor

--- contrib/terraform/openstack/modules/compute/nodes.tf
+++ contrib/terraform/openstack/modules/compute/nodes.tf
@@ -37,3 +37,123 @@ module "nodes_sm2" {
   secgroups                                    = ["${openstack_networking_secgroup_v2.k8s.name}", "${openstack_networking_secgroup_v2.worker.name}"]
   supplementary_node_groups                    = "${var.supplementary_node_groups}"
 }
+
+module "nodes_sm3" {
+  source = "./despegar_nodes"
+
+  node_type                                    = "sm3"
+  cluster_name                                 = "${var.cluster_name}"
+  az_list                                      = "${var.az_list}"
+  az_list_node                                 = "${var.az_list_node}"
+  number_of_k8s_nodes                          = "${values(var.despegar_k8s_nodes)[2]}"
+  node_root_volume_size_in_gb                  = "${var.node_root_volume_size_in_gb}"
+  image                                        = "${var.image}"
+  ssh_user                                     = "${var.ssh_user}"
+  flavor_k8s_node                              = "${keys(var.despegar_k8s_nodes)[2]}"
+  network_name                                 = "${var.network_name}"
+  network_id                                   = "${var.network_id}"
+  use_access_ip                                = "${var.use_access_ip}"
+  key_pair                                     = "${openstack_compute_keypair_v2.k8s.name}"
+  secgroups                                    = ["${openstack_networking_secgroup_v2.k8s.name}", "${openstack_networking_secgroup_v2.worker.name}"]
+  supplementary_node_groups                    = "${var.supplementary_node_groups}"
+}
+
+module "nodes_sm4" {
+  source = "./despegar_nodes"
+
+  node_type                                    = "sm4"
+  cluster_name                                 = "${var.cluster_name}"
+  az_list                                      = "${var.az_list}"
+  az_list_node                                 = "${var.az_list_node}"
+  number_of_k8s_nodes                          = "${values(var.despegar_k8s_nodes)[3]}"
+  node_root_volume_size_in_gb                  = "${var.node_root_volume_size_in_gb}"
+  image                                        = "${var.image}"
+  ssh_user                                     = "${var.ssh_user}"
+  flavor_k8s_node                              = "${keys(var.despegar_k8s_nodes)[3]}"
+  network_name                                 = "${var.network_name}"
+  network_id                                   = "${var.network_id}"
+  use_access_ip                                = "${var.use_access_ip}"
+  key_pair                                     = "${openstack_compute_keypair_v2.k8s.name}"
+  secgroups                                    = ["${openstack_networking_secgroup_v2.k8s.name}", "${openstack_networking_secgroup_v2.worker.name}"]
+  supplementary_node_groups                    = "${var.supplementary_node_groups}"
+}
+
+module "nodes_sm5" {
+  source = "./despegar_nodes"
+
+  node_type                                    = "sm5"
+  cluster_name                                 = "${var.cluster_name}"
+  az_list                                      = "${var.az_list}"
+  az_list_node                                 = "${var.az_list_node}"
+  number_of_k8s_nodes                          = "${values(var.despegar_k8s_nodes)[4]}"
+  node_root_volume_size_in_gb                  = "${var.node_root_volume_size_in_gb}"
+  image                                        = "${var.image}"
+  ssh_user                                     = "${var.ssh_user}"
+  flavor_k8s_node                              = "${keys(var.despegar_k8s_nodes)[4]}"
+  network_name                                 = "${var.network_name}"
+  network_id                                   = "${var.network_id}"
+  use_access_ip                                = "${var.use_access_ip}"
+  key_pair                                     = "${openstack_compute_keypair_v2.k8s.name}"
+  secgroups                                    = ["${openstack_networking_secgroup_v2.k8s.name}", "${openstack_networking_secgroup_v2.worker.name}"]
+  supplementary_node_groups                    = "${var.supplementary_node_groups}"
+}
+
+module "nodes_sm6" {
+  source = "./despegar_nodes"
+
+  node_type                                    = "sm6"
+  cluster_name                                 = "${var.cluster_name}"
+  az_list                                      = "${var.az_list}"
+  az_list_node                                 = "${var.az_list_node}"
+  number_of_k8s_nodes                          = "${values(var.despegar_k8s_nodes)[5]}"
+  node_root_volume_size_in_gb                  = "${var.node_root_volume_size_in_gb}"
+  image                                        = "${var.image}"
+  ssh_user                                     = "${var.ssh_user}"
+  flavor_k8s_node                              = "${keys(var.despegar_k8s_nodes)[5]}"
+  network_name                                 = "${var.network_name}"
+  network_id                                   = "${var.network_id}"
+  use_access_ip                                = "${var.use_access_ip}"
+  key_pair                                     = "${openstack_compute_keypair_v2.k8s.name}"
+  secgroups                                    = ["${openstack_networking_secgroup_v2.k8s.name}", "${openstack_networking_secgroup_v2.worker.name}"]
+  supplementary_node_groups                    = "${var.supplementary_node_groups}"
+}
+
+module "nodes_sm7" {
+  source = "./despegar_nodes"
+
+  node_type                                    = "sm7"
+  cluster_name                                 = "${var.cluster_name}"
+  az_list                                      = "${var.az_list}"
+  az_list_node                                 = "${var.az_list_node}"
+  number_of_k8s_nodes                          = "${values(var.despegar_k8s_nodes)[6]}"
+  node_root_volume_size_in_gb                  = "${var.node_root_volume_size_in_gb}"
+  image                                        = "${var.image}"
+  ssh_user                                     = "${var.ssh_user}"
+  flavor_k8s_node                              = "${keys(var.despegar_k8s_nodes)[6]}"
+  network_name                                 = "${var.network_name}"
+  network_id                                   = "${var.network_id}"
+  use_access_ip                                = "${var.use_access_ip}"
+  key_pair                                     = "${openstack_compute_keypair_v2.k8s.name}"
+  secgroups                                    = ["${openstack_networking_secgroup_v2.k8s.name}", "${openstack_networking_secgroup_v2.worker.name}"]
+  supplementary_node_groups                    = "${var.supplementary_node_groups}"
+}
+
+module "nodes_sm8" {
+  source = "./despegar_nodes"
+
+  node_type                                    = "sm8"
+  cluster_name                                 = "${var.cluster_name}"
+  az_list                                      = "${var.az_list}"
+  az_list_node                                 = "${var.az_list_node}"
+  number_of_k8s_nodes                          = "${values(var.despegar_k8s_nodes)[7]}"
+  node_root_volume_size_in_gb                  = "${var.node_root_volume_size_in_gb}"
+  image                                        = "${var.image}"
+  ssh_user                                     = "${var.ssh_user}"
+  flavor_k8s_node                              = "${keys(var.despegar_k8s_nodes)[7]}"
+  network_name                                 = "${var.network_name}"
+  network_id                                   = "${var.network_id}"
+  use_access_ip                                = "${var.use_access_ip}"
+  key_pair                                     = "${openstack_compute_keypair_v2.k8s.name}"
+  secgroups                                    = ["${openstack_networking_secgroup_v2.k8s.name}", "${openstack_networking_secgroup_v2.worker.name}"]
+  supplementary_node_groups                    = "${var.supplementary_node_groups}"
+}
