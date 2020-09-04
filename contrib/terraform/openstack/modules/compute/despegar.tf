feb643ed495f4030be7246c0e94314275e9b0bd7
Gustavo Randich <gustavo.randich@gmail.com>
2020-09-04 12:58:28 -0300

    tidy despegar resources

---
+++ contrib/terraform/openstack/modules/compute/despegar.tf
@@ -0,0 +1,42 @@
+# poder excluir los master nodes del service CIDR advertising
+#   desde rack 69 fisicos no llego a http://10.232.23.94:8080 cuando es extTraffPolicy Cluster, porque rutea por el master que tiene security group restringido
+#   y ademas esta mal que pase por el master
+#   https://github.com/projectcalico/calico/issues/1604
+#   https://github.com/projectcalico/calico/issues/3365#issuecomment-660202663
+resource "openstack_networking_secgroup_rule_v2" "master_tcp" {
+  security_group_id = "${openstack_networking_secgroup_v2.k8s_master.id}"
+  count             = "${length(var.worker_allowed_ports)}"
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "${lookup(var.worker_allowed_ports[count.index], "protocol", "tcp")}"
+  port_range_min    = "${lookup(var.worker_allowed_ports[count.index], "port_range_min")}"
+  port_range_max    = "${lookup(var.worker_allowed_ports[count.index], "port_range_max")}"
+  remote_ip_prefix  = "${lookup(var.worker_allowed_ports[count.index], "remote_ip_prefix", "0.0.0.0/0")}"
+}
+
+data "template_file" "user_data" {
+  template = file("${path.module}/despegar_nodes/user-data.tpl")
+
+  vars = {
+    cluster_domain = var.cluster_domain
+  }
+}
+
+# allow ping
+resource "openstack_networking_secgroup_rule_v2" "k8s_icmp_i" {
+  count             = "${length(var.k8s_allowed_remote_ips)}"
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "icmp"
+  remote_ip_prefix  = "${var.k8s_allowed_remote_ips[count.index]}"
+  security_group_id = "${openstack_networking_secgroup_v2.k8s.id}"
+}
+
+resource "openstack_networking_secgroup_rule_v2" "k8s_icmp_e" {
+  count             = "${length(var.k8s_allowed_remote_ips)}"
+  direction         = "egress"
+  ethertype         = "IPv4"
+  protocol          = "icmp"
+  remote_ip_prefix  = "${var.k8s_allowed_remote_ips[count.index]}"
+  security_group_id = "${openstack_networking_secgroup_v2.k8s.id}"
+}
