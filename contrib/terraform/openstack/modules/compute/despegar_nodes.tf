5cf1f5dfa6c1c3d32a6ea7db78e05e563b0c2b61
Gustavo Randich <grandich@despegar.com>
2021-03-03 12:48:41 -0300

    new node types

--- contrib/terraform/openstack/modules/compute/despegar_nodes.tf
+++ contrib/terraform/openstack/modules/compute/despegar_nodes.tf
@@ -189,3 +189,51 @@ module "nodes_type8" {
   dns_zone                                     = "${var.dns_zone}"
   cluster_domain                               = "${var.cluster_domain}"
 }
+
+module "nodes_type9" {
+  source = "./despegar_nodes"
+
+  node_type                                    = "t09"
+  cluster_name                                 = "${var.cluster_name}"
+  az_list                                      = "${var.az_list}"
+  az_list_node                                 = "${var.az_list_node}"
+  number_of_k8s_nodes                          = "${var.despegar_k8s_nodes[8]["q"]}"
+  node_root_volume_size_in_gb                  = "${var.node_root_volume_size_in_gb}"
+  image                                        = "${var.image}"
+  ssh_user                                     = "${var.ssh_user}"
+  flavor_k8s_node                              = "${var.despegar_k8s_nodes[8]["flavor"]}"
+  network_name                                 = "${var.network_name}"
+  network_id                                   = "${var.network_id}"
+  use_access_ip                                = "${var.use_access_ip}"
+  key_pair                                     = "${openstack_compute_keypair_v2.k8s.name}"
+  secgroups                                    = ["${openstack_networking_secgroup_v2.k8s.name}", "${openstack_networking_secgroup_v2.worker.name}"]
+  supplementary_node_groups                    = "${var.supplementary_node_groups}"
+  kube_service_addresses                       = "${var.kube_service_addresses}"
+  kube_pods_subnet                             = "${var.kube_pods_subnet}"
+  dns_zone                                     = "${var.dns_zone}"
+  cluster_domain                               = "${var.cluster_domain}"
+}
+
+module "nodes_type10" {
+  source = "./despegar_nodes"
+
+  node_type                                    = "t10"
+  cluster_name                                 = "${var.cluster_name}"
+  az_list                                      = "${var.az_list}"
+  az_list_node                                 = "${var.az_list_node}"
+  number_of_k8s_nodes                          = "${var.despegar_k8s_nodes[9]["q"]}"
+  node_root_volume_size_in_gb                  = "${var.node_root_volume_size_in_gb}"
+  image                                        = "${var.image}"
+  ssh_user                                     = "${var.ssh_user}"
+  flavor_k8s_node                              = "${var.despegar_k8s_nodes[9]["flavor"]}"
+  network_name                                 = "${var.network_name}"
+  network_id                                   = "${var.network_id}"
+  use_access_ip                                = "${var.use_access_ip}"
+  key_pair                                     = "${openstack_compute_keypair_v2.k8s.name}"
+  secgroups                                    = ["${openstack_networking_secgroup_v2.k8s.name}", "${openstack_networking_secgroup_v2.worker.name}"]
+  supplementary_node_groups                    = "${var.supplementary_node_groups}"
+  kube_service_addresses                       = "${var.kube_service_addresses}"
+  kube_pods_subnet                             = "${var.kube_pods_subnet}"
+  dns_zone                                     = "${var.dns_zone}"
+  cluster_domain                               = "${var.cluster_domain}"
+}
