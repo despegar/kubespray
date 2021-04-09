04089c9ed45c32d3350c03efd90dfc99398f5654
Gustavo Randich <grandich@despegar.com>
2021-04-09 19:06:38 -0300

    interpolations

--- contrib/terraform/openstack/modules/compute/despegar.tf
+++ contrib/terraform/openstack/modules/compute/despegar.tf
@@ -4,14 +4,14 @@
 #   https://github.com/projectcalico/calico/issues/1604
 #   https://github.com/projectcalico/calico/issues/3365#issuecomment-660202663
 resource "openstack_networking_secgroup_rule_v2" "master_tcp" {
-  security_group_id = "${openstack_networking_secgroup_v2.k8s_master.id}"
-  count             = "${length(var.worker_allowed_ports)}"
+  security_group_id = openstack_networking_secgroup_v2.k8s_master.id
+  count             = length(var.worker_allowed_ports)
   direction         = "ingress"
   ethertype         = "IPv4"
-  protocol          = "${lookup(var.worker_allowed_ports[count.index], "protocol", "tcp")}"
-  port_range_min    = "${lookup(var.worker_allowed_ports[count.index], "port_range_min")}"
-  port_range_max    = "${lookup(var.worker_allowed_ports[count.index], "port_range_max")}"
-  remote_ip_prefix  = "${lookup(var.worker_allowed_ports[count.index], "remote_ip_prefix", "0.0.0.0/0")}"
+  protocol          = lookup(var.worker_allowed_ports[count.index], "protocol", "tcp")
+  port_range_min    = lookup(var.worker_allowed_ports[count.index], "port_range_min")
+  port_range_max    = lookup(var.worker_allowed_ports[count.index], "port_range_max")
+  remote_ip_prefix  = lookup(var.worker_allowed_ports[count.index], "remote_ip_prefix", "0.0.0.0/0")
 }
 
 data "template_file" "user_data" {
@@ -24,53 +24,53 @@ data "template_file" "user_data" {
 
 # allow ping
 resource "openstack_networking_secgroup_rule_v2" "k8s_icmp_i" {
-  count             = "${length(var.k8s_allowed_remote_ips)}"
+  count             = length(var.k8s_allowed_remote_ips)
   direction         = "ingress"
   ethertype         = "IPv4"
   protocol          = "icmp"
-  remote_ip_prefix  = "${var.k8s_allowed_remote_ips[count.index]}"
-  security_group_id = "${openstack_networking_secgroup_v2.k8s.id}"
+  remote_ip_prefix  = var.k8s_allowed_remote_ips[count.index]
+  security_group_id = openstack_networking_secgroup_v2.k8s.id
 }
 
 resource "openstack_networking_secgroup_rule_v2" "k8s_icmp_e" {
-  count             = "${length(var.k8s_allowed_remote_ips)}"
+  count             = length(var.k8s_allowed_remote_ips)
   direction         = "egress"
   ethertype         = "IPv4"
   protocol          = "icmp"
-  remote_ip_prefix  = "${var.k8s_allowed_remote_ips[count.index]}"
-  security_group_id = "${openstack_networking_secgroup_v2.k8s.id}"
+  remote_ip_prefix  = var.k8s_allowed_remote_ips[count.index]
+  security_group_id = openstack_networking_secgroup_v2.k8s.id
 }
 
 # allow SNMP
 resource "openstack_networking_secgroup_rule_v2" "k8s_snmp" {
-  count             = "${length(var.k8s_allowed_remote_ips)}"
+  count             = length(var.k8s_allowed_remote_ips)
   direction         = "ingress"
   ethertype         = "IPv4"
   protocol          = "udp"
   port_range_min    = 161
   port_range_max    = 161
-  remote_ip_prefix  = "${var.k8s_allowed_remote_ips[count.index]}"
-  security_group_id = "${openstack_networking_secgroup_v2.k8s.id}"
+  remote_ip_prefix  = var.k8s_allowed_remote_ips[count.index]
+  security_group_id = openstack_networking_secgroup_v2.k8s.id
 }
 
 # allow NTP from nagios
 resource "openstack_networking_secgroup_rule_v2" "k8s_ntp" {
-  count             = "${length(var.k8s_allowed_remote_ips)}"
+  count             = length(var.k8s_allowed_remote_ips)
   direction         = "ingress"
   ethertype         = "IPv4"
   protocol          = "udp"
   port_range_min    = 123
   port_range_max    = 123
-  remote_ip_prefix  = "${var.k8s_allowed_remote_ips[count.index]}"
-  security_group_id = "${openstack_networking_secgroup_v2.k8s.id}"
+  remote_ip_prefix  = var.k8s_allowed_remote_ips[count.index]
+  security_group_id = openstack_networking_secgroup_v2.k8s.id
 }
 resource "openstack_networking_secgroup_rule_v2" "k8s_ntp_e" {
-  count             = "${length(var.k8s_allowed_remote_ips)}"
+  count             = length(var.k8s_allowed_remote_ips)
   direction         = "egress"
   ethertype         = "IPv4"
   protocol          = "udp"
   port_range_min    = 123
   port_range_max    = 123
-  remote_ip_prefix  = "${var.k8s_allowed_remote_ips[count.index]}"
-  security_group_id = "${openstack_networking_secgroup_v2.k8s.id}"
+  remote_ip_prefix  = var.k8s_allowed_remote_ips[count.index]
+  security_group_id = openstack_networking_secgroup_v2.k8s.id
 }
--- contrib/terraform/openstack/modules/compute/despegar_nodes/main.tf
+++ contrib/terraform/openstack/modules/compute/despegar_nodes/main.tf
@@ -1,6 +1,6 @@
 resource "null_resource" "dns_updater_probe" {
  triggers = {
-    always_run = "${timestamp()}"
+    always_run = timestamp()
   }
   provisioner "local-exec" {
     command = <<EOT
@@ -32,20 +32,20 @@ data "template_file" "user_data" {
 
 resource "openstack_compute_instance_v2" "k8s_despegar_node" {
   name              = "${var.cluster_name}-node-${var.node_type}-${count.index + 1}"
-  count             = "${var.number_of_k8s_nodes}"
-  availability_zone = "${element(var.az_list_node, count.index)}"
-  image_name        = "${var.image}"
-  flavor_id         = "${var.flavor_k8s_node}"
-  key_pair          = "${var.key_pair}"
+  count             = var.number_of_k8s_nodes
+  availability_zone = element(var.az_list_node, count.index)
+  image_name        = var.image
+  flavor_id         = var.flavor_k8s_node
+  key_pair          = var.key_pair
 
   depends_on = [ null_resource.dns_updater_probe ]
 
   dynamic "block_device" {
     for_each = var.node_root_volume_size_in_gb > 0 ? [var.image] : []
     content {
-      uuid                  = "${data.openstack_images_image_v2.vm_image.id}"
+      uuid                  = data.openstack_images_image_v2.vm_image.id
       source_type           = "image"
-      volume_size           = "${var.node_root_volume_size_in_gb}"
+      volume_size           = var.node_root_volume_size_in_gb
       boot_index            = 0
       destination_type      = "volume"
       delete_on_termination = true
@@ -53,7 +53,7 @@ resource "openstack_compute_instance_v2" "k8s_despegar_node" {
   }
 
   network {
-    name = "${var.network_name}"
+    name = var.network_name
   }
 
   security_groups = var.secgroups
@@ -64,12 +64,12 @@ resource "openstack_compute_instance_v2" "k8s_despegar_node" {
   }
 
   metadata = {
-    ssh_user                 = "${var.ssh_user}"
+    ssh_user                 = var.ssh_user
     kubespray_groups         = "kube-node,k8s-cluster,no-floating,${var.supplementary_node_groups}"
-    depends_on               = "${var.network_id}"
-    use_access_ip            = "${var.use_access_ip}"
-    kube_service_addresses   = "${var.kube_service_addresses}"
-    kube_pods_subnet         = "${var.kube_pods_subnet}"
+    depends_on               = var.network_id
+    use_access_ip            = var.use_access_ip
+    kube_service_addresses   = var.kube_service_addresses
+    kube_pods_subnet         = var.kube_pods_subnet
   }
 
   provisioner "local-exec" {
